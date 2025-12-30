9176270d26984969d84388ed54124d834c91e8f6
AllPoland <52187288+AllPoland@users.noreply.github.com>
2025-12-29 18:54:37 -0700

    fill missing songs
    all items !

--- worlds/unbeatable_arcade/songs.py
+++ worlds/unbeatable_arcade/songs.py
@@ -8,8 +8,9 @@ base_songs = [
     {"name":"ball is infinite", "b":3, "n":8, "h":11, "e":13, "u":18, "s":-1},
     {"name":"bang", "b":5, "n":7, "h":10, "e":14, "u":18, "s":23},
     {"name":"beyond the heart (broken heart mix)", "b":2, "n":3, "h":8, "e":10, "u":14, "s":-1},
+    {"name":"blank page", "b":4, "n":9, "h":12, "e":15, "u":16, "s":19},
     {"name":"bookend song", "b":5, "n":10, "h":13, "e":17, "u":23, "s":-1},
-    {"name":"bookend song - opening ver. -", "b":2, "n":5, "h":7, "e":10, "u":12, "s":-1},
+    {"name":"bookend song -opening ver.-", "b":2, "n":5, "h":7, "e":10, "u":12, "s":-1},
     {"name":"call_home", "b":1, "n":5, "h":7, "e":10, "u":12, "s":-1},
     {"name":"ch2_dream1", "b":2, "n":6, "h":9, "e":11, "u":12, "s":-1},
     {"name":"ch2_dream2", "b":2, "n":5, "h":8, "e":10, "u":12, "s":-1},
@@ -20,7 +21,8 @@ base_songs = [
     {"name":"cosmic starter", "b":2, "n":7, "h":10, "e":13, "u":20, "s":-1},
     {"name":"dialtone", "b":2, "n":8, "h":12, "e":15, "u":19, "s":-1},
     {"name":"disco disaster", "b":4, "n":7, "h":9, "e":12, "u":17, "s":-1},
-    {"name":"done in love (from noisz sl)", "b":2, "n":7, "h":11, "e":14, "u":18, "s":-1},
+    {"name":"done in love (from noisz sl)", "b":2, "n":7, "h":11, "e":14, "u":18, "s":22},
+    {"name":"done in love (full version)", "b":1, "n":5, "h":8, "e":11, "u":19, "s":20},
     {"name":"drastic hammer", "b":3, "n":6, "h":9, "e":11, "u":13, "s":-1},
     {"name":"eating is living", "b":4, "n":6, "h":8, "e":11, "u":14, "s":22},
     {"name":"empty diary", "b":1, "n":4, "h":8, "e":10, "u":14, "s":-1},
@@ -42,6 +44,7 @@ base_songs = [
     {"name":"memorized", "b":3, "n":7, "h":10, "e":12, "u":15, "s":-1},
     {"name":"mirror", "b":2, "n":8, "h":9, "e":11, "u":16, "s":16},
     {"name":"mirror - kamome sano remix", "b":4, "n":9, "h":11, "e":13, "u":21, "s":-1},
+    {"name":"motherbound", "b":6, "n":9, "h":11, "e":15, "u":20, "s":22},
     {"name":"neo elsewhere", "b":4, "n":7, "h":9, "e":12, "u":16, "s":-1},
     {"name":"no signal", "b":3, "n":7, "h":9, "e":12, "u":16, "s":-1},
     {"name":"om and on", "b":5, "n":9, "h":12, "e":15, "u":20, "s":22},
@@ -56,11 +59,13 @@ base_songs = [
     {"name":"silent spiral", "b":4, "n":8, "h":11, "e":14, "u":21, "s":-1},
     {"name":"sleeping in", "b":2, "n":5, "h":11, "e":14, "u":18, "s":-1},
     {"name":"stolen", "b":2, "n":7, "h":10, "e":13, "u":17, "s":-1},
-    {"name":"the adventures of clef the great, the coolest person you've ever met in your entire life, who you are only a speck of dust next to in the grand scheme of things, featuring treble sometimes. an-please, dear god, clef, this is not the final title of the song. we cannot put this on there. first of all, it's too long, and second of all, it's... i mean, this isn't a title. this is barely a collection of words you said out loud. uh yeah why is that a problem. it's... you know what, sure, yeah, okay, label guy, put this on there. literally print every word in our conversation. do you have a maximum length? you don't? what, really? we have other songs on this record. what's that g... you know what? no, it's fine. we're gonna do it, and keep this tape so that when she complains about it i can show her the evidence.", "b":3, "n":7, "h":9, "e":11, "u":14, "s":-1},
+    {"name":"the adventures of clef the great, the coolest person you've ever met in your entire life, who you are only a speck of dust next to in the grand scheme of things, featuring treble sometimes. an-please, dear god, clef, this is not the final title of the song. we cannot put this on there. first of all, it's too long, and second of all, it's...i mean, this isn't a title. this is barely a collection of words you said out loud. uh yeah why is that a problem. it's...you know what, sure. yeah, okay, label guy, put this on there. literally print every word in our conversation. do you have a maximum length? you don't? what, really? we have other songs on this record. what's that g...you know what? no, it's fine. we're gonna do it. and keep this tape so that when she complains about it i can show her the evidence.", "b":3, "n":7, "h":9, "e":11, "u":14, "s":-1},
     {"name":"true (from noisz sl)", "b":5, "n":8, "h":10, "e":13, "u":21, "s":23},
-    {"name":"twist sound", "b":4, "n":7, "h":9, "e":14, "u":17, "s":-1},
+    {"name":"true (full version)", "b":2, "n":6, "h":9, "e":13, "u":18, "s":22},
+    {"name":"twist sound", "b":4, "n":7, "h":9, "e":14, "u":17, "s":19},
     {"name":"waiting", "b":2, "n":5, "h":8, "e":10, "u":14, "s":21},
     {"name":"waiting - lumena-tan remix", "b":1, "n":4, "h":7, "e":14, "u":19, "s":20},
+    {"name":"waiting (live)", "b":2, "n":5, "h":9, "e":10, "u":14, "s":-1},
     {"name":"war den", "b":5, "n":9, "h":12, "e":15, "u":19, "s":-1},
     {"name":"worn out tapes", "b":3, "n":6, "h":9, "e":13, "u":16, "s":-1},
     {"name":"worn out tapes [tally-ho! version]", "b":5, "n":7, "h":11, "e":14, "u":21, "s":25},
@@ -84,12 +89,12 @@ breakout_songs = [
     {"name":"welcome to slamtown", "b":2, "n":7, "h":10, "e":13, "u":19, "s":-1},
 ]
 
-# Contains alternate names for each song for hinting/searching
+# Contains alternate names for some songs for hinting/searching
 song_aliases = [
     {"name":"beyond the heart (broken heart mix)", "aliases":["beyond the heart"]},
     {"name":"done in love (from noisz sl)", "aliases":["done in love"]},
     {"name":"k.moe (vip)", "aliases":["k.moe"]},
-    {"name":"the adventures of clef the great, the coolest person you've ever met in your entire life, who you are only a speck of dust next to in the grand scheme of things, featuring treble sometimes. an-please, dear god, clef, this is not the final title of the song. we cannot put this on there. first of all, it's too long, and second of all, it's... i mean, this isn't a title. this is barely a collection of words you said out loud. uh yeah why is that a problem. it's... you know what, sure, yeah, okay, label guy, put this on there. literally print every word in our conversation. do you have a maximum length? you don't? what, really? we have other songs on this record. what's that g... you know what? no, it's fine. we're gonna do it, and keep this tape so that when she complains about it i can show her the evidence.", "aliases":["the adventures of clef the great"]},
+    {"name":"the adventures of clef the great, the coolest person you've ever met in your entire life, who you are only a speck of dust next to in the grand scheme of things, featuring treble sometimes. an-please, dear god, clef, this is not the final title of the song. we cannot put this on there. first of all, it's too long, and second of all, it's...i mean, this isn't a title. this is barely a collection of words you said out loud. uh yeah why is that a problem. it's...you know what, sure. yeah, okay, label guy, put this on there. literally print every word in our conversation. do you have a maximum length? you don't? what, really? we have other songs on this record. what's that g...you know what? no, it's fine. we're gonna do it. and keep this tape so that when she complains about it i can show her the evidence.", "aliases":["the adventures of clef the great"]},
 ]
 
 # All the songs in the entire game, this is used for defining IDs
